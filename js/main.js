b98cda793e674079fffb65ecff8abd932e28c51e
sina mansoory <sinaman7979@gmail.com>
2022-02-15 18:15:50 -0800

    add function completedTask to define the task that has been completed.

--- js/main.js
+++ js/main.js
@@ -3,21 +3,30 @@ const inputForm = document.querySelector(".todo_input");
 const tasksUl = document.getElementsByClassName("tasks-list")[0];
 
 button.addEventListener("click", addTask);
+let indexLi = 0
+let indexButton = 0;
 
 function addTask(event) {
     event.preventDefault()
+    
     const tasksDiv = document.createElement("div");
     tasksDiv.classList = "tasks-container"
 
     const tasksLi = document.createElement("li");
     tasksDiv.appendChild(tasksLi);
-    tasksLi.classList = "tasks"
+    tasksLi.classList = "tasks";
+    tasksLi.id = indexLi++
     
+
+    //check button 
     const checkButton = document.createElement("button");
     checkButton.innerHTML = `<i class="fa-solid fa-check"></i>`
     checkButton.classList = "check-btn "
+    checkButton.id = indexButton++;
     tasksDiv.appendChild(checkButton)
+    checkButton.addEventListener("click", completedTask)
 
+    //trash button     
     const trashButton = document.createElement("button");
     trashButton.innerHTML = `<i class="fa-solid fa-trash"></i>`
     trashButton.classList = "trash-btn"
@@ -49,3 +58,8 @@ function addTask(event) {
     }
 }
 
+function completedTask(event) {
+     let selectedTask = document.getElementsByClassName("tasks-list")[0].getElementsByClassName("tasks-container")[event.target.id].childNodes[0];
+
+     selectedTask.classList = "completed";
+}
